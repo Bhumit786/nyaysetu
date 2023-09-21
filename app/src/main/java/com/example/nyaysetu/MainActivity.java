f25006ae0a02b2d1c89babef558b899f983e56ea
Bhumit786 <Bhumit2020csiot024@abesit.edu.in>
2023-09-21 22:48:44 +0530

    second

--- app/src/main/java/com/example/nyaysetu/MainActivity.java
+++ app/src/main/java/com/example/nyaysetu/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -13,13 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frontscreen);
-        logoimage = (ImageView) findViewById(R.id.logoimage);
+        logoimage = findViewById(R.id.logoimage);
         logoimage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openNewActivity();
             }
         });
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                // TODO: Your application init goes here.
+                Intent mInHome = new Intent(MainActivity.this, admin_login.class);
+                MainActivity.this.startActivity(mInHome);
+                MainActivity.this.finish();
+            }
+        }, 3000);
     }
     public void openNewActivity(){
         Intent intent = new Intent(this,admin_login.class);
